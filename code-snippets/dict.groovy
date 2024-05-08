b64e5e499f1b0f5e1cb90bc89d793aae46f1f54b
Krish <98233937+krishpy3@users.noreply.github.com>
2024-05-08 18:19:37 +0000

    TEST

--- code-snippets/dict.groovy
+++
@@ -1,3 +0,0 @@
-def keys = [mysql, nginx-http, nginx-https, apache-http, apache-https, sesnu-client, php-fpm]
-def values = ['3306', '80', '443', '80', '443', '3030', '9000' ]
-[keys,values].transpose().collectEntries()
